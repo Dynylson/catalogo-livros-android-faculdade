f23d952880016a1c412b4f4f5f1e16bdbd8cf49a
Dynylson Júnior <dynylsonjr@gmail.com>
2026-01-25 14:49:42 -0300

    feat: add global error handler

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import bookRoutes from './routes/books/routes';
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/books', bookRoutes);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
